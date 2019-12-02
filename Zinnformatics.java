d571aaccd8122706f3389efcf877ede841c7dfa2
ejhon1116 <ejhon1116@gmail.com>
2019-12-02 08:19:51 -0500

    fixed the conditions for discount method and fixed the final price calculator

--- Zinnformatics.java
+++ Zinnformatics.java
@@ -5,7 +5,7 @@ public class Zinnformatics
     {
         String comp = JOptionPane.showInputDialog(null, "This program will allow you to place an order at Zinnformatics.\nPlease enter your company name.", "Zinnformatics", JOptionPane.QUESTION_MESSAGE);
         int quantity = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the amount of products you would like to buy.", "Zinnformatics", JOptionPane.QUESTION_MESSAGE));
-        JOptionPane.showMessageDialog(null, "Thank you for your order " +  comp + "! You have order " + quantity + " packages at a " + discount(quantity) * 100 + "% discount. Your final cost will be " + zinnformatics(quantity) * discount(quantity) + ".", "Zinnformatics", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Thank you for your order " +  comp + "! You have order " + quantity + " packages at a " + discount(quantity) * 100 + "% discount. Your final cost will be $" + zinnformatics(quantity) * (1 - discount(quantity)) + "0.", "Zinnformatics", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static int zinnformatics(int quantity)
@@ -15,10 +15,10 @@ public class Zinnformatics
     
     public static double discount(int quantity)
     {
-        if(quantity > 99) return 0.5;
-        else if(quantity > 49) return 0.4;
-        else if(quantity > 49) return 0.4;
-        else if(quantity > 9) return 0.2;
+        if(quantity >= 100) return 0.5;
+        else if(quantity >= 50) return 0.4;
+        else if(quantity >= 20) return 0.3;
+        else if(quantity >= 10) return 0.2;
         return 0.0;
     }
 }
