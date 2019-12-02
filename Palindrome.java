b0ac546a91b83e648334d29fe9a9ae920288fca7
ejhon1116 <ejhon1116@gmail.com>
2019-12-02 08:37:37 -0500

    Commented it

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,34 @@
+/* Palindrome.java | Author: Ethan Jhong *
+ * Determines whether a user inputted    *
+ * string is a palindrome                */
 import javax.swing.JOptionPane;
 public class Palindrome
 {
   public static void main(String[] args)
   {
     int rep;
+    
+    //asks the user if they want to use the program
     if(JOptionPane.showConfirmDialog(null, "This program checks if a string is a palindrome. Would you like to use it?", "Palindrome Checker", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE) == 0) rep = 1;
     else rep = 0;
     while(rep > 0)
     {
       rep --;
+      
+      //asks the user to input a string to be checked
       String str = JOptionPane.showInputDialog(null, "Please enter a string to be checked", "Palindrome Checker", JOptionPane.QUESTION_MESSAGE);
+      
+      //determines whether the string is or isnt a palindrome
       if(isPalindrome(str) == true) JOptionPane.showMessageDialog(null, "Your string is a palindrome!", "Palindrome Checker", JOptionPane.INFORMATION_MESSAGE);
       else JOptionPane.showMessageDialog(null, "Your string is not a palindrome", "Palindrome Checker", JOptionPane.INFORMATION_MESSAGE);
+     
+      //asks the user if they want to use the program again
       if(JOptionPane.showConfirmDialog(null, "Would you like to use the program again?", "Palindrome Checker", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0) rep = 1;
       System.out.println(rep);
     }
   }
-/////////////////////////////////////////////////////
+  
+  //reverses an inputted string
   public static String reverseString(String str)
   {
     String res = "";
@@ -24,7 +36,8 @@ public class Palindrome
       res = res + str.charAt(length);
     return res;
   }
-/////////////////////////////////////////////////////
+  
+  //determines whether a string is the same as it is when it is backwards
   public static boolean isPalindrome(String str)
   {
     if(str.equalsIgnoreCase(reverseString(str))) return true;
