09cbdc15f7735b142b1fe8515d5d6c4421d3ebdf
ejhon1116 <ejhon1116@gmail.com>
2019-12-01 20:35:43 -0500

    Fixed a thing

--- Palindrome.java
+++ Palindrome.java
@@ -1,17 +1,19 @@
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 public class Palindrome
 {
     public static void main(String[] args)
     {
         int rep = 1;
-        while(rep > 0)
-        {
-            rep --;
-            JOptionPane.showMessageDialog(null, "Input String", "dfsfisjisjfis");
-            //if(isPalindrome(str) == true) JOptionPane.showMessageDialog(null, str + " is a palindrome!");
-            //else JOptionPane.showMessageDialog(null, str + " is not a palindrome!");
-            JOptionPane.showConfirmDialog(null, "Would you like to reuse the program");
-        }
+            int yn = JOptionPane.showConfirmDialog(null, "This is a program is a palindrome tester. Would you like to use it?", "Palindrome", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            while(yn == 0 && rep == 1)
+            {
+                rep --;
+                String str = JOptionPane.showInputDialog(null, "Please input a string to be checked.", "Palindrome", JOptionPane.QUESTION_MESSAGE);
+                if(isPalindrome(str) == true) JOptionPane.showMessageDialog(null, str + " is a palindrome!");
+                else JOptionPane.showMessageDialog(null, str + " is not a palindrome!");
+                if(JOptionPane.showConfirmDialog(null, "Would you like to reuse the program?", "Palindrome", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0) rep = 1;
+            }
     }
     public static String reverseString(String str)
     {
